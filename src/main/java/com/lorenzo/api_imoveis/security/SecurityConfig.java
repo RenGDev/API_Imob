e4d72dad7095c6058d8f159ab7e64a2984f51bd2
Lorenzo <lorenzodequadrosgoncalves@gmail.com>
2025-07-02 00:03:43 -0300

    ja não sei mais

--- src/main/java/com/lorenzo/api_imoveis/security/SecurityConfig.java
+++ src/main/java/com/lorenzo/api_imoveis/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://imob-website-front-xu8t-i6ome4f7x-lorenzqgs-projects.vercel.app/")); // Seu frontend
+        config.setAllowedOrigins(List.of("https://imob-website-front-xu8t-i6ome4f7x-lorenzqgs-projects.vercel.app")); // Seu frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
